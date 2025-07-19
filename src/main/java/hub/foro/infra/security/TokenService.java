e60efc16860db26b7a8b9e5895143573c8931ef9
Andres Gutierrez <anfegumos2005@gmail.com>
2025-07-18 23:45:02 -0500

    Autorizacion mediante TokenJWT

--- src/main/java/hub/foro/infra/security/TokenService.java
+++ src/main/java/hub/foro/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package hub.foro.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import hub.foro.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -32,5 +33,19 @@ public class TokenService {
 
     private Instant fechaExpiracion() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("ForoHub")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("TokenJWT invalido o expirado!", exception);
+        }
     }
 }
